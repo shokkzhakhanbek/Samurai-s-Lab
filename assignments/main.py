aa312d46ef0b8b2ed899b7b249ce337717d9f2a8
shokkzhakhanbek <makhkambayev.s04@gmail.com>
2025-11-30 20:50:42 +0800

    new homework

---
+++ assignments/main.py
@@ -0,0 +1,157 @@
+# Assignment 6 
+
+# 1 Exersise 
+x = 10 
+y = 5 
+
+result = x+y 
+print=(result)
+
+# 2 Exersise
+text = "Samaurai"
+
+reserved_text = text[::-1]
+print(reserved_text)
+
+# 3 Exersise
+text = "samurai"
+
+length = len(text)
+print(length)
+
+# 4 Exersise
+word1 = "samurai"
+word2 = "Tanos"
+
+full = word1 + " " + word2
+print(full)
+
+# 5 Exersise
+word = "a"
+
+if word.lower() in "aeiou":
+    print(True)
+else:
+    print(False)
+
+
+# 6 Exersise
+text = "samurai"
+
+first = text[0]
+last = text[-1]
+middle = text[1:-1]
+
+new_text = last + middle + first
+
+print(new_text)
+
+
+# 7 Exersise
+text = "samurai"
+
+upper_text = text.upper()
+print(upper_text)
+
+# 8 Exersise
+len = 15
+width = 10
+
+area = len * width
+print(area)
+
+# 9 Exersise
+number = 7
+if number % 2 == 0:
+    print("Even")
+
+
+# 10 Exersise
+text = "samurai"
+
+extract = text[0:3]
+print(extract)
+
+# 11 Exersise
+name = "Samurai"
+age = "25"
+
+info = f"My name is {name} and I am {age} years old."
+print(info)
+
+# 12 Exersise
+text = "samurai"
+
+extract = text[2:6]
+print(extract)
+
+# 13 Exersise
+number_str = ("12345")
+number_int = int(number_str)
+
+print(number_int)
+
+#14 Exersise
+text = "samurai"
+
+repeated_text = text * 3
+print(repeated_text)
+
+# 15 Exersise
+a = 5
+b = 10
+
+quatient = b // a
+remainder = b % a
+
+print(quatient)
+print(remainder)
+
+# 16 Exersise
+a = 6 
+b = 3
+
+float_result = a * b
+print(float_result)
+
+
+# 17 Exersise
+text = "samurai"
+
+count_a = text.count("a")
+print(count_a)
+
+# 18 Exersise
+text = "samurai said: \"be brave!\""
+
+print(text)
+
+# 19 Exersise
+names = """ Alice
+Bob
+Charlie 
+David """
+
+print(names)
+
+# 20 Exersise
+base = 5
+exponent = 3
+
+power = base ** exponent
+print(power)
+
+# 21 Exersise
+word = 'level'
+
+is_palindrome = word == word[::-1]
+print(is_palindrome)
+
+# 22 Exersise
+text1 = "listen"
+text2 = "silent"
+
+are_anagrams = sorted(text1) == sorted(text2)
+print(are_anagrams)
+
+
